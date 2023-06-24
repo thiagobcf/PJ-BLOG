e42904e755896ec3b59a3f86ca2b372b9e985223
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-06-24 11:30:08 -0300

    conf. o django-summernote

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -21,8 +21,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('blog.urls')),
+    path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
-]
+
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(
