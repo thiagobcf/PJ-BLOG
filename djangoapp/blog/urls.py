91973fadb66793ce34bcafd96320d10da06bc12b
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-06-26 12:08:09 -0300

    filtrando e ordenando os posts

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,10 +1,10 @@
+from blog.views import index, page, post
 from django.urls import path
-from blog.views import index, post, page
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('post/', post, name='post'),
+    path('post/<slug:slug>/', post, name='post'),
     path('page/', page, name='page'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,10 +1,28 @@
+from blog.models import Post
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+PER_PAGE = 9
 
 
 def index(request):
+    posts = (
+        Post
+        .objects
+        .filter(is_published=True)
+        .order_by('-pk')
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
     )
 
 
@@ -12,6 +30,9 @@ def page(request):
     return render(
         request,
         'blog/pages/page.html',
+        {
+            # 'page_obj': page_obj,
+        }
     )
 
 
@@ -19,4 +40,7 @@ def post(request):
     return render(
         request,
         'blog/pages/post.html',
+        {
+            # 'page_obj': page_obj,
+        }
     )
