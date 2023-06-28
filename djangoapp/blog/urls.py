dcc751434cf078d8e8c7c27ea877bca2e2cf47e0
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-06-28 19:32:11 -0300

    migrando a view category + allow_empty para 404

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,15 +1,19 @@
-from blog.views import PostListViews, page, post, created_by, category, tag, search
+from blog.views import (CreatedByListView, PostListView, category, page, post,
+                        search, tag)
 from django.urls import path
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', PostListViews.as_view(), name='index'),
+    path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path(
+        'created_by/<int:author_pk>/',
+        CreatedByListView.as_view(),
+        name='created_by'
+    ),
     path('category/<slug:slug>/', category, name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
     path('search/', search, name='search'),
-
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,26 +1,22 @@
-from blog.models import Post, Page
-from django.core.paginator import Paginator
-from django.shortcuts import render
-from django.db.models import Q
+from typing import Any
+
+from blog.models import Page, Post
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.db.models.query import QuerySet
 from django.http import Http404
-from django.views.generic.list import ListView
+from django.shortcuts import render
+from django.views.generic import ListView
 
 PER_PAGE = 9
 
 
-class PostListViews(ListView):
-    model = Post
+class PostListView(ListView):
     template_name = 'blog/pages/index.html'
-    context_object_name = 'page_obj'
-    ordering = '-pk',
+    context_object_name = 'posts'
     paginate_by = PER_PAGE
     queryset = Post.objects.get_published()
-
-    # def get_queryset(self):
-    #     queryset = super().get_queryset()
-    #     queryset = queryset.filter(is_published=True)
-    #     return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -30,22 +26,6 @@ class PostListViews(ListView):
         })
 
         return context
-
-# def index(request):
-#     posts = Post.objects.get_published()
-
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': 'Home - ',
-#         }
-#     )
 
 
 def created_by(request, author_pk):
@@ -72,9 +52,48 @@ def created_by(request, author_pk):
         {
             'page_obj': page_obj,
             'page_title': page_title,
-
         }
     )
+
+
+class CreatedByListView(PostListView):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._temp_context: dict[str, Any] = {}
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
+        user_full_name = user.username
+
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
+        page_title = 'Posts de ' + user_full_name + ' - '
+
+        ctx.update({
+            'page_title': page_title,
+        })
+
+        return ctx
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return qs
+
+    def get(self, request, *args, **kwargs):
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk).first()
+
+        if user is None:
+            raise Http404()
+
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+
+        return super().get(request, *args, **kwargs)
 
 
 def category(request, slug):
@@ -96,7 +115,6 @@ def category(request, slug):
         {
             'page_obj': page_obj,
             'page_title': page_title,
-
         }
     )
 
@@ -120,7 +138,6 @@ def tag(request, slug):
         {
             'page_obj': page_obj,
             'page_title': page_title,
-
         }
     )
 
@@ -146,7 +163,6 @@ def search(request):
             'page_obj': posts,
             'search_value': search_value,
             'page_title': page_title,
-
         }
     )
 
@@ -158,6 +174,7 @@ def page(request, slug):
         .filter(slug=slug)
         .first()
     )
+
     if page_obj is None:
         raise Http404()
 
@@ -169,7 +186,6 @@ def page(request, slug):
         {
             'page': page_obj,
             'page_title': page_title,
-
         }
     )
 
@@ -192,6 +208,5 @@ def post(request, slug):
         {
             'post': post_obj,
             'page_title': page_title,
-
         }
     )
