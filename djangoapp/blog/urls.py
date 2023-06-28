4fdaa70aad372a726595474421ac8ecc0dd70c14
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-06-28 12:41:17 -0300

    Migrando a views index

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,10 +1,10 @@
-from blog.views import index, page, post, created_by, category, tag, search
+from blog.views import PostListViews, page, post, created_by, category, tag, search
 from django.urls import path
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', PostListViews.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -4,25 +4,48 @@ from django.shortcuts import render
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.http import Http404
+from django.views.generic.list import ListView
 
 PER_PAGE = 9
 
 
-def index(request):
-    posts = Post.objects.get_published()
+class PostListViews(ListView):
+    model = Post
+    template_name = 'blog/pages/index.html'
+    context_object_name = 'page_obj'
+    ordering = '-pk',
+    paginate_by = PER_PAGE
+    queryset = Post.objects.get_published()
 
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(is_published=True)
+    #     return queryset
 
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({
             'page_title': 'Home - ',
-        }
-    )
+        })
+
+        return context
+
+# def index(request):
+#     posts = Post.objects.get_published()
+
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': 'Home - ',
+#         }
+#     )
 
 
 def created_by(request, author_pk):
